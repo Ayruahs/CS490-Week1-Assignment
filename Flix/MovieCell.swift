17cecaa6212314cbe525399b2ff26850368b09c3
Shaurya Sinha <ShauryaSinha@ASHUTOSHs-MacBook-Pro.local>
2018-01-14 22:23:17 -0500

    Finished Assignment

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Shaurya Sinha on 14/01/18.
+//  Copyright © 2018 Shaurya Sinha. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class NowPlayingViewController: UIViewController {
-
+class NowPlayingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        
+        tableView.insertSubview(refreshControl, at: 0)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        fetchMovies()
+        
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
+        
+        fetchMovies()
+        //self.activityIndicator.stopAnimating()
+        
+        
+    }
+    
+    func fetchMovies(){
+        self.activityIndicator.startAnimating()
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=4aebe566ef8c2257a0ca6a337f1994ef")
         let request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration:  .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -22,15 +49,43 @@ class NowPlayingViewController: UIViewController {
             } else if let data = data{
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String : Any]
                 let movies = dataDictionary["results"] as! [[String : Any]]
-                for movie in movies{
-                    let title = movie["title" ] as! String
-                    print(title )
-                }
+                self.movies = movies
+                self.tableView.reloadData()
+                self.activityIndicator.stopAnimating()
             }
+            self.refreshControl.endRefreshing()
         }
         task.resume()
         
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         
+        return movies.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] 
+        let overview = movie["overview"]
+        
+        cell.titleLabel.text = (title as! String)
+        cell.overviewLabel.text = (overview as! String)
+        
+        let posterPathString = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
+        let posterURL = URL(string: baseURLString + posterPathString)!
+        
+        cell.posterImageView.af_setImage(withURL: posterURL)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 260
     }
 
     override func didReceiveMemoryWarning() {
